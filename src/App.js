9c2f38e83d7b2247083d665fb1e5580471950400
HardiB.Salih <hardib.salih@gmail.com>
2023-01-23 01:49:36 +0300

    Search implementation is complete/

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   constructor() {
+    console.log("constructor");
     super();
     this.state = {
-      monstors: []
-    }
+      monstors: [],
+      searchField: "",
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
+    console.log("componentDidMount");
     // When the page load we do api request
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(
-      ()=> {
-        return {monstors : users}
-    },
-    () => {
-      console.log(this.state)
-    }
-    ))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monstors: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
+
+  onSearchChanges = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
 
   render() {
+    console.log("render");
+
+    const { monstors, searchField } = this.state;
+    const { onSearchChanges } = this;
+
+    const filteredMonstor = monstors.filter((monstor) => {
+      return monstor.name.toLocaleLowerCase().includes(searchField);
+    });
     return (
       <div className="App">
-        {this.state.monstors.map((monstor) => (
+        <input
+          className="searchBox"
+          type="search"
+          placeholder="Search Monstors"
+          onChange={onSearchChanges}
+        />
+        {filteredMonstor.map((monstor) => (
           <div key={monstor.id}>
             <p>{monstor.name}</p>
           </div>
@@ -35,7 +60,6 @@ class App extends Component {
       </div>
     );
   }
-  
 }
 
 export default App;
